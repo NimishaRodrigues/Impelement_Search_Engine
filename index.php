9bbdb911ce503c33b0484b250597b65d4f03641e
Nimisha Rodrigues <nr57@buffalo.edu>
2018-04-07 18:39:02 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>searchengine</title>
+</head>
+<body>
+<form action='search.php' method='GET'>
+
+<center>
+<br><br><br><br><br><br><br>
+<h1>TECHUNTERR</h1>
+<br> <br> <br>
+<input type='text' size='90' name='search'></br></br><br><br>
+<input type='submit' name='submit' value='SEARCH' ></br></br></br>
+</center>
+</form>
+</body>
+</html>
